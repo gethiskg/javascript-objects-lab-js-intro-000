d65c7e48eb4fd4df5977781189e3f47dd6ef3f42
Ismaiilov Atai <atai2497@gmail.com>
2020-05-31 00:29:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
      return Object.assign({}, object, {[key]: value})
 };
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object.key = value
+  object[key] = value
   return object;
 
 }
