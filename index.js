48d3b596d1e4831eb473560c0471c616f066ef8f
Ismaiilov Atai <atai2497@gmail.com>
2020-05-31 00:46:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,3 +33,7 @@ function deleteFromObjectByKey(object, key){
    delete newObj[key]
    return newObj
 }
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete Object[key]
+  return Object;
+}
