da88b9b984f7d218c03af169b6c9b10dd78df338
Ismaiilov Atai <atai2497@gmail.com>
2020-05-31 00:39:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,5 +31,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 function deleteFromObjectByKey(object, key){
    var newObj = object.assign({} object)
    delete newObj[key]
-   return newObj;
+   return newObj
 }
